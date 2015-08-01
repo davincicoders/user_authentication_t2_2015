5c2b80c1ce67dd08e72c9b1d901376b250baa527
Jason Noble <github+jasonn@jasonnoble.org>
2015-07-31 18:52:51 -0600

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/about', as: 'about'
+  get 'pages/contact', as: 'contact'
+
   resources :cars
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  # The priority ics based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'cars#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
